d686c20855cdd2500fd6d7e12eaf8db455fea31b
Saurabh Gurde <saurabhgurde@gmail.com>
2025-02-19 00:34:58 +0530

    vercel config added

---
+++ server/api/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.js";
+import fetch from "./middleware/auth.js";
+import bodyParser from "body-parser";
+import data from "./routes/data.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use("/", userRoute);
+app.use("/data",fetch, data);
+
+export default app
